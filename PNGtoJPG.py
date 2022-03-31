f5da4a783cdd362cb1bcdbf1c427a1b1e1087d8a
Kubsy <kubadd475@gmail.com>
2022-03-31 18:39:05 +0100

    Initialize Tkinter

---
+++ PNGtoJPG.py
@@ -0,0 +1,12 @@
+from curses import window
+from tkinter import *
+
+Window = Tk()
+Window.title("PNGtoJPG")
+Window.geometry("600x600") 
+
+class app():
+
+    def __init__(self, main):
+        
+        
