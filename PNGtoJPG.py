4ef5a6880e45ddbeff8017f4d82dc4a6635ee461
Kubsy <kubadd475@gmail.com>
2022-04-01 18:55:06 +0100

    add commentary (to seperate widgets) and resize listbox

--- PNGtoJPG.py
+++ PNGtoJPG.py
@@ -14,11 +14,14 @@ class app():
     def createWidgets(self, main):
         mainFrame = tk.Frame(main)
 
+        #Labels
         self.Heading = tk.Label(mainFrame, text="Welcome to the \n PNG to JPG converter!", font="Bold", pady=10).grid(row=0, column=1)
+
+        #Buttons
         self.Convert = tk.Button(mainFrame, text="Convert to JPG", pady=5).grid(row=1, column=2)
         self.importJPGButton = tk.Button(mainFrame, text="Import a PNG image", pady=5, command=self.getImage).grid(row=1, column=0)
 
-        self.list = tk.Listbox(mainFrame).grid(row=1,column=1)
+        self.list = tk.Listbox(mainFrame, height=5).grid(row=1,column=1)
 
         mainFrame.pack()
 
