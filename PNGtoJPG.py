1b2f9de7c3bd9c2832bf84b5a080d30c75e83f9d
Kubsy <kubadd475@gmail.com>
2022-04-10 13:08:34 +0100

    Introduce PIL

--- PNGtoJPG.py
+++ PNGtoJPG.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
-
+from PIL import Image
 
 Window = tk.Tk()
 Window.resizable(False, False)
@@ -18,7 +18,7 @@ class app():
         self.Heading = tk.Label(mainFrame, text="Welcome to the \n PNG to JPG converter!", font="Bold", pady=10).grid(row=0, column=1)
 
         #Buttons
-        self.Convert = tk.Button(mainFrame, text="Convert to JPG", pady=5).grid(row=1, column=2)
+        self.Convert = tk.Button(mainFrame, text="Convert to JPG", pady=5, command=self.convert).grid(row=1, column=2)
         self.importJPGButton = tk.Button(mainFrame, text="Import a PNG image", pady=5, command=self.getImage).grid(row=1, column=0)
 
         self.list = tk.Listbox(mainFrame, height=5).grid(row=1,column=1)
@@ -26,7 +26,11 @@ class app():
         mainFrame.pack()
 
     def getImage(self):
+        global image_temp
+
         self.filename =  tk.filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
+        image_temp = Image.open(self.filename).convert()
+
 
     def convert(self):
         pass
